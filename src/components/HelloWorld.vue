71fb16f7bd99558938b7502a5c16e9d549b3cccc
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-10-26 18:25:49 -0500

    pagina

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,225 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <header class="header">
+      <h1 class="title">UITEC App</h1>
+      
+    </header>
+    <main>
+      <div class="table-container">
+        <p>Creado por <a href="">Andres MORETA</a> 2023</p>
+        <h2>Equipos</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Nombres</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Computadora</td>
+            </tr>
+            <tr>
+              <td>Arduino</td>
+            </tr>
+            <tr>
+              <td>Router</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="table-container">
+        <h2>Estudiantes</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Equipo</th>
+              <th>Estado</th>
+              <th>Fecha Adquisición</th>
+              <th>Fecha Máxima</th>
+              <th>Fecha Devuelto</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(usuario, index) in usuarios" :key="index">
+              <td>{{ usuario.nombre }}</td>
+              <td>{{ usuario.equipo }}</td>
+              <td :class="usuario.estado">{{ usuario.estado }}</td>
+              <td>{{ usuario.fechaAdquisicion }}</td>
+              <td>{{ usuario.fechaMaxima }}</td>
+              <td>{{ usuario.fechaDevuelto }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="table-container">
+        <h2>Pagos Pendientes</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Estudiante</th>
+              <th>Equipo</th>
+              <th>Total a Pagar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(usuario, index) in usuarios" :key="index">
+              <td>{{ usuario.nombre }}</td>
+              <td>{{ usuario.equipo }}</td>
+              <td>$ {{ usuario.deuda }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+    <footer>
+     
+    </footer>
+    <div class="input-form">
+      <h2>Add a Student</h2>
+      <form @submit.prevent="addStudent">
+        <label for="nombre">Nombre:</label>
+        <input type="text" v-model="newStudent.nombre" required><br><br>
+        <label for="equipo">Equipo:</label>
+        <input type="text" v-model="newStudent.equipo" required><br><br>
+        <label for="estado">Estado:</label>
+        <input type="text" v-model="newStudent.estado" required><br><br>
+        <label for="fechaAdquisicion">Fecha Adquisición:</label>
+        <input type="text" v-model="newStudent.fechaAdquisicion" required><br><br>
+        <label for="fechaMaxima">Fecha Máxima:</label>
+        <input type="text" v-model="newStudent.fechaMaxima" required><br><br>
+        <label for="fechaDevuelto">Fecha Devuelto:</label>
+        <input type="text" v-model="newStudent.fechaDevuelto" required><br><br>
+        <button type="submit" class="add-button">Add Student</button><br><br>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HomeVue",
+  data() {
+    return {
+      usuarios: [
+        // Your existing data
+      ],
+      newStudent: {
+        nombre: "",
+        equipo: "",
+        estado: "",
+        fechaAdquisicion: "",
+        fechaMaxima: "",
+        fechaDevuelto: "",
+        deuda: 0,
+      },
+    };
+  },
+  methods: {
+    CalcularDeuda() {
+      // Your existing method
+    },
+    addStudent() {
+      // Calculate deuda for the new student and push them to the usuarios array
+      const fecha = new Date(this.newStudent.fechaMaxima);
+      const fechaDevuelto = new Date(this.newStudent.fechaDevuelto);
+      this.newStudent.deuda = this.CalcularDeuda(fecha, fechaDevuelto);
+      this.usuarios.push({ ...this.newStudent });
+      // Reset the form
+      this.newStudent = {
+        nombre: "",
+        equipo: "",
+        estado: "",
+        fechaAdquisicion: "",
+        fechaMaxima: "",
+        fechaDevuelto: "",
+        deuda: 0,
+      };
+    },
+  },
+  created() {
+    this.usuarios.forEach((element) => {
+      const fecha = new Date(element.fechaMaxima);
+      const fechaDevuelto = new Date(element.fechaDevuelto);
+      element.deuda = this.CalcularDeuda(fecha, fechaDevuelto);
+    });
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  background-color: white;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.header {
+  background-color: #1590b8;
+  text-align: center;
+  padding: 20px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.title {
+  font-size: 2rem;
+  color: white;
 }
+
+.table-container {
+  margin-top: 5%;
+  text-align: center;
+}
+
+.table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: auto;
+}
+
+table th,
+table td {
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+
+th {
+  background-color: #1590b8;
+  color: white;
+}
+
+td {
+  padding: 10px 15px !important;
+}
+
+.Entregado {
+  background-color: green;
+  color: #FFF;
+}
+
+.Pendiente {
+  background-color: red;
+  color: #FFF;
+}
+
+footer {
+  margin-top: 10%;
+  text-align: center;
+}
+
 a {
-  color: #42b983;
+  color: royalblue;
+}
+
+.add-button {
+  background-color: #1590b8;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 5px 2px;
+  cursor: pointer;
+  border-radius: 5px;
 }
 </style>
